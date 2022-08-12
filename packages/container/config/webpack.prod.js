7d2a8421e8da5b52b487530c179aaefca6c65db2
vikash patel <vikashbhut23@gmail.com>
2022-08-12 11:32:04 +0530

    modified webpack

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -2,6 +2,8 @@ const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFedartionPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
+
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
